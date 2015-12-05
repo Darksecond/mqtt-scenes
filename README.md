7a63a69cc8a0a77d8f63573ccce5059ce11f21de
Tim Peters <mail@darksecond.nl>
2015-12-05 16:26:51 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# MQTT Scenes
+
+This is MQTT based Scene manager. It will perform commands when triggered.
+
+```
+scenes/{uniqueId}/set/trigger
+  {
+    "trigger": true
+  }
+```
+
+Is how you trigger a scene.
--- test.js
+++ test.js
@@ -47,23 +47,3 @@ mqttRegex.subscribe('sensors/hue/00:17:88:01:10:39:27:ce-02-fc00/get/buttons/dow
   console.log("down");
   mqttClient.publish('scenes/bedroom-dark/set/trigger', '{"trigger": true}');
 });
-
-let setLights = function(newState) {
-  if(newState) {
-    mqttClient.publish('scenes/bedroom-on/set/trigger', '{"trigger": true}');
-  } else {
-    mqttClient.publish('scenes/bedroom-off/set/trigger', '{"trigger": true}');
-  }
-};
-
-let setLightsBrightness = function(brightness) {
-  let newState = JSON.stringify({
-    on: true,
-    brightness: brightness
-  });
-
-  mqttClient.publish('lights/hue/00:17:88:01:00:f5:b3:6c-0b/set/state', String(newState)); // Middle
-  mqttClient.publish('lights/hue/00:17:88:01:00:dd:1b:73-0b/set/state', String(newState)); // Window
-  mqttClient.publish('lights/hue/00:17:88:01:00:10:6e:17-0b/set/state', String(newState)); // TV
-  mqttClient.publish('lights/hue/00:17:88:01:00:10:6e:15-0b/set/state', String(newState)); // Bed
-};
